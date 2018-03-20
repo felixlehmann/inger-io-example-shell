7a91028695eb39485c6755edcca0acaac71f47f7
Felix Lehmann <felix.lehmann@applicata.de>
2018-03-20 10:14:52 +0100

    Added example files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # inger-io-example-shell
 Inger.io example for shell usage
+
+Examples use jq to format JSON output. If it's not installed, the response will be printed as plain text.
+
+Usage:
+git clone https://github.com/felixlehmann/inger-io-example-shell.git
+cd example && ./curl.sh v1 google adwords v201710 published
---
+++ example/curl.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#set -x #echo on
+
+# Example call: ./curl.sh v1 google adwords v201710 published
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ]; then
+    inger_version="v1"
+    vendor="facebook"
+    service="graph"
+    version="v2.10"
+    method="deprecated"
+else
+    inger_version=$1
+    vendor=$2
+    service=$3
+    version=$4
+    method=$5
+fi
+
+api_url="http://inger.io/$inger_version/$vendor/$service/$version/$method"
+if [ -x "$(command -v jq)" ]; then
+    deprecation_date=source curl -s $api_url --header 'content-type: application/json' | jq
+else
+    # jq for JSON pretty print not available
+    deprecation_date=source curl -s $api_url --header 'content-type: application/json'
+fi
+
+echo $deprecation_date
